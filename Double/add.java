6617062145f9cd9337fda6689d05131d7aaad3b7
TusharPawar123 <tusharpawar82018@gmail.com>
2023-05-17 11:04:07 +0530

    my fifth commit

---
+++ Double/add.java
@@ -0,0 +1,17 @@
+package Double;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 double a8 = 56.5;
+		    double a7 = 56.9;
+		    double a6 = 78.7;
+		    double a4 = 87.9;
+		    double a5 = 99.9;
+		    		
+		    double average = (double)(a8+a7+a6+a4+a5)/5;	
+		    System.out.println(average);
+	}
+
+}
